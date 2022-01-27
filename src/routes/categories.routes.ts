2c74ea15c839193e494cbcc7454d505a7a0ef3a0
augustoroocha <augustinho_darocha1311@hotmail.com>
2022-01-27 14:55:13 -0200

    repositories categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,16 @@
 import { Router } from "express";
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
-const categoriesRoutes = Router()
-
-const categoryes: Category[] = [];
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
-    const { name, description } = request.body
+    const { name, description } = request.body;
 
-    const category = new Category()
+    categoriesRepository.created({ name, description })
 
-    Object.assign(category, {
-        name,
-        description,
-        created_at: new Date()
-    })
 
-    categoryes.push(category)
-
-    return response.status(201).json({ categoryes })
+    return response.status(201).send()
 })
 
 export { categoriesRoutes }
