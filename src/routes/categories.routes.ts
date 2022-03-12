58b778239a4ebf3d0842fbd85f21727e689f476f
augustoroocha <augustinho_darocha1311@hotmail.com>
2022-03-12 14:31:20 -0300

    lendo arquivos csv/multer rota import criada

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-
+import multer from 'multer';
 import { CategoriesRepository } from '../modules/cars/repositories/implementations/CategoriesRepository';
 import { createCategoryController } from '../modules/cars/useCases/createCategory/index'
 import { listCategoriesController } from '../modules/cars/useCases/listCategories/index';
@@ -7,7 +7,10 @@ import { deleteCategoryController } from '../modules/cars/useCases/deleteCategor
 import { listCategoryByIDController } from '../modules/cars/useCases/listCategoryByID';
 
 const categoriesRoutes = Router();
-const categoriesRepository = CategoriesRepository.getInstance();
+
+const upload = multer({
+    dest: "./tmp"
+});
 
 // Receber a Requisição
 // Chamar o Serviço
@@ -28,5 +31,9 @@ categoriesRoutes.delete("/:id", (request, response) => {
     return deleteCategoryController.handle(request, response);
 })
 
-
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+    const { file } = request;
+    console.log(file);
+    return response.send();
+})
 export { categoriesRoutes }
