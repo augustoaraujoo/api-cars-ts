61af9f90321d09233eb85de91a914e140eed5444
augustoroocha <augustinho_darocha1311@hotmail.com>
2022-07-04 11:56:18 -0300

    resolvendo bug do typeorm ao iniciar a migration

--- src/shared/infra/typeorm/migrations/1650642208418-addUserT.ts
+++ src/shared/infra/typeorm/migrations/1650642208418-addUserT.ts
@@ -3,6 +3,8 @@ import { MigrationInterface, QueryRunner, Table } from "typeorm";
 export class CreateUsers1650310684622 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+
         await queryRunner.createTable(
             new Table({
                 name: "users",
